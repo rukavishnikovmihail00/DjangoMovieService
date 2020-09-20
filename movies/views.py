5e07954dc6d677ae6036c09846fd24bdc9031e6e
rukavishnikovmihail00 <61332555+rukavishnikovmihail00@users.noreply.github.com>
2020-09-20 18:49:01 +0300

    Minor additions

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Movie
 from django.views.generic.base import View
+from django.views.generic import DetailView
 # Create your views here.
 
 class MoviesView(View):
@@ -13,3 +14,5 @@ class MovieDetailView(View):
     def get(self, request, slug):
         movie = Movie.objects.get(url=slug, draft=False)
         return render(request, "movies/movie_detail.html", {"movie": movie})
+
+
